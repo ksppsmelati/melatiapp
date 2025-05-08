e527b2248ef5fd592290124c7e5179489695343e
KSPPS MELATI <it@bmtmelati.com>
2025-05-08 15:53:44 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://app.bmtmelati.com";
+$config['base_url'] = "https://melatiapp.bmtmelati.com";
 // $config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
 // $config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 
